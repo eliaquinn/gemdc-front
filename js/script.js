72f5444dfb47341a06100ab3a4396f9caef83eb4
Eliaquin Araújo <eliaquin.j.araujo@gmail.com>
2022-04-27 15:34:18 -0400

    creatign html structure of files

--- js/script.js
+++ js/script.js
@@ -1,17 +1,54 @@
 const btnLogin = document.querySelector('.btn-login')
 const menuUser = document.querySelector('.menu-user')
 
-class DocumentSettings {
+class Client {
     constructor () {
-        this.arrElements = []
+        this.elementArr = []
     }
 
     insertElement (data) {
-        this.arrElements.push(data)
+        this.elementArr.push(data)
     }
 
     userMenu () {
-        this.arrElements.filter(item => {
+        let btnUser = this.elementArr.filter(item => item.classList.contains('btn-login'))
+
+        document.addEventListener('click', this.menuList.bind(this))
+    }
+
+    menuList (event) {
+        if(event.target.classList.contains('btn-login')) {
+            menuUser.innerHTML = ''
+
+            if(menuUser.style.display === 'block') {
+                menuUser.style.display = ''
+                return
+            }
+
+            menuUser.style.display = 'block'
+            menuUser.innerHTML = `<span>Brian Costa</span>`
+            let ul = document.createElement('ul')
+            ul.innerHTML = `
+                <li><a href="#"><i class='bx bxs-user-badge'></i>Perfil</a></li>
+                <li><a href="#"><i class='bx bxs-user-detail'></i>Configurações</a></li>
+                <li><a href="../../index.html"><i class='bx bx-exit' ></i>Sair</a></li>
+                `
+            menuUser.appendChild(ul)
+            return
+        }
+        
+        menuUser.style.display = ''
+    }
+}
+
+const user = new Client()
+
+user.insertElement(btnLogin)
+
+user.userMenu()
+
+/*
+    {
             if(item.classList.value == 'btn-login') {
                 item.onclick = () => {
                     menuUser.innerHTML = ''
@@ -20,21 +57,15 @@ class DocumentSettings {
                         return
                     }
                     menuUser.style.display = 'block'
+                    menuUser.innerHTML = `<span>Brian Costa</span>`
                     let ul = document.createElement('ul')
                     ul.innerHTML = `
                         <li>Perfil</li>
                         <li>Configurações</li>
-                        <li>Sair</li>
+                        <li><a href="../../index.html">Sair</a></li>
                     `
                     menuUser.appendChild(ul)
                 }
             }
-        })
-    }
-}
-
-const userLogin = new DocumentSettings()
-
-userLogin.insertElement(btnLogin)
-
-userLogin.userMenu()
+        }
+*/
